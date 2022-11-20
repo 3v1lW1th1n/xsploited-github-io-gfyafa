f8629b68032824c09a8e66744aa875f7dade2d1c
3v1lW1th1n <nolteg@outlook.com>
2022-11-20 15:38:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# xsploited-github-io-gfyafa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/xsploited-github-io-gfyafa)
---
+++ src/app/cdk-menu-menubar-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/** @title Google Docs Menu Bar. */
+@Component({
+  selector: 'cdk-menu-menubar-example',
+  exportAs: 'cdkMenuMenubarExample',
+  styleUrls: ['cdk-menu-menubar-example.css'],
+  templateUrl: 'cdk-menu-menubar-example.html',
+})
+export class CdkMenuMenubarExample {}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
